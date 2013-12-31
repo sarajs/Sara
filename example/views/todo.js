f334ed30d7c5bc4dcc3dda110911887ef420e165
Jackson Gariety <personal@jacksongariety.com>
2013-12-31 14:39:17 -0800

    Server-side-rendering of AngularJS example works!

--- example/views/todo.js
+++ example/views/todo.js
@@ -3,5 +3,38 @@ var app = require('../app')
   , SaraAngular = require('sara-angular')
 
 exports.render = function () {
-  return SaraAngular(fs.readFileSync(app.root + '/templates/index.html').toString())
+  return SaraAngular(fs.readFileSync(app.root + '/templates/index.html').toString(), function TodoView($scope) {
+    $scope.todos = [
+      { title:'learn angular', complete: true },
+      { title:'build an angular app', complete: false }
+    ];
+   
+    $scope.addTodo = function() {
+      $scope.todos.push({ title: $scope.todo.title, complete: false });
+      $scope.todo.title = '';
+    };
+    
+    $scope.$evalAsync(function() {
+      var prerendered = document.querySelectorAll("[data-prerendered]")
+      for (var content = prerendered.length; content--;) {
+        prerendered[content].parentNode.removeChild(prerendered[content])
+      }
+    })
+   
+    $scope.active = function() {
+      var count = 0;
+      angular.forEach($scope.todos, function(todo) {
+        count += todo.complete ? 0 : 1;
+      });
+      return count;
+    };
+   
+    $scope.archive = function() {
+      var oldTodos = $scope.todos;
+      $scope.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.complete) $scope.todos.push(todo);
+      });
+    };
+  })
 }
